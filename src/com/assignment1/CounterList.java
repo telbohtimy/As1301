b60fed5393b59d0422c67d982a5ea66a980f0631
telbohtimy <elbohtim@ualberta.ca>
2014-01-31 12:06:18 -0700

    not sure whats wrong

--- src/com/assignment1/CounterList.java
+++ src/com/assignment1/CounterList.java
@@ -13,11 +13,10 @@ import java.util.ArrayList;
 import java.util.Calendar;
 import java.util.Collections;
 
-
-import android.os.Bundle;
 import android.app.Activity;
 import android.content.Context;
 import android.content.Intent;
+import android.os.Bundle;
 import android.view.View;
 import android.view.View.OnClickListener;
 import android.view.ViewGroup.LayoutParams;
